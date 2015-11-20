d2d4c302c208008ccc9c756febeb7d0b015fc406
HannibalA <Alawode.Hannibal@gmail.com>
2015-11-20 02:55:31 -0500

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,26 @@
+$(".btn-success").on("click", function(e) {
+    e.preventDefault();
+
+var newWord, newDate, newNotes, deleteButton, deleteTd;
+
+newWord = $("#newWord").val();
+newDate = $("#newDate").val();
+newNotes = $("#newNotes").val();
+    newRow = $("<tr>");
+    wordTd = $("<td>").addClass("word-td").append(newWord);
+    wordTdd = $("<td>").addClass("word-td").append(newDate);
+    wordTddd = $("<td>").addClass("word-td").append(newNotes);
+    deleteButton = $("<button>").addClass("btn btn-danger").append("Delete");
+    deleteTd = $("<td>").append(deleteButton);
+
+    newRow.append(wordTd).append(wordTdd).append(wordTddd).append(deleteTd);
+
+    $("tbody").append(newRow);
+    $("#newWord").val("");
+    $("#newDate").val("");
+    $("#newNotes").val("");
+  });
+
+$("table").on("click", ".btn-danger", function() {
+    $(this).parents("tr").remove();
+  });
