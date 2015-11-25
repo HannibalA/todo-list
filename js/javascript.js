1dfb8d2d57d9d4b8f2d3a9b98dca0b63fc95272b
HannibalA <Alawode.Hannibal@gmail.com>
2015-11-24 20:06:10 -0500

    changed title and added features

--- js/javascript.js
+++ js/javascript.js
@@ -6,6 +6,17 @@ var newWord, newDate, newNotes, deleteButton, deleteTd;
 newWord = $("#newWord").val();
 newDate = $("#newDate").val();
 newNotes = $("#newNotes").val();
+
+      if (newWord.length === 0) {
+      $("#nothing").fadeIn(2000);
+      return;
+    };
+
+    if (newDate.length === 0) {
+      $("#nothing2").fadeIn(2000);
+      return;
+    };
+
     newRow = $("<tr>");
     wordTd = $("<td>").addClass("word-td").append(newWord);
     wordTdd = $("<td>").addClass("word-td").append(newDate);
@@ -24,3 +35,8 @@ newNotes = $("#newNotes").val();
 $("table").on("click", ".btn-danger", function() {
     $(this).parents("tr").remove();
   });
+
+$("#newWord,#newDate").on("keydown", function() {
+    $(".alert").slideUp();
+  });
+
